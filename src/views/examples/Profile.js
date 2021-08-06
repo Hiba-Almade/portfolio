cc944c8df5ba4c8f1e5623a0e40bcc23215a0073
Hiba-Almade <hibaalmade998@gmail.com>
2021-08-06 15:05:42 +0300

    add personal info

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -75,7 +75,7 @@ class Profile extends React.Component {
                           <img
                             alt="..."
                             className="rounded-circle"
-                            src={profile}
+                            src='https://media-exp1.licdn.com/dms/image/C4D03AQEcLfG8S8ze5g/profile-displayphoto-shrink_400_400/0/1621873653077?e=1633564800&v=beta&t=jUksI7nfGQObCt-bmDhFncv8e3TNB9zuaJ_rBdWbfP8'
                           />
                         </a>
                       </div>
@@ -108,7 +108,7 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
+                          <span className="heading">30</span>
                           <span className="description">Friends</span>
                         </div>
                         <div>
@@ -116,7 +116,7 @@ class Profile extends React.Component {
                           <span className="description">Photos</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
+                          <span className="heading">20</span>
                           <span className="description">Comments</span>
                         </div>
                       </div>
@@ -124,31 +124,30 @@ class Profile extends React.Component {
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
+                     Hiba Almade{" "}
                       <span className="font-weight-light">, 27</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Damascus, Syria
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Engineer - Full-Stack dev
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      Zarqa Uni
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                        A software developer with a world that is 
+                        developed and built by computers, you will see my fingerprint
+                         and the fingerprint of my programmer friends everywhere..
+                        I can draw and love to read, with a cup of coffee the world becomes a paradise
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
